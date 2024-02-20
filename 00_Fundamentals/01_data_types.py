db54288f6b016335a43780cd44c37aed8d2a8cae
MykeSelivanov <michael777selivanov@gmail.com>
2024-02-19 22:39:33 -0600

    data types

---
+++ 00_Fundamentals/01_data_types.py
@@ -0,0 +1,17 @@
+# int 6 9 -9 0 277456
+
+# float 2.3 4.5 -0.9 -.3
+
+# str "hello" 'world' '2' "12345" ' %^&*(&)'
+
+# bool True False
+
+# None - signify emptiness
+
+print("Hello")
+print(4)
+print(type(5))
+print(type(5.123))
+print(type(True))
+print(type(" string "))
+print(type(None))
