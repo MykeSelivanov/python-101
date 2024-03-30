77700e5de032b2ac46f12a11390a5d232bf7f470
MykeSelivanov <michael777selivanov@gmail.com>
2024-03-30 00:01:48 -0500

    basics

--- 00_Fundamentals/02_variables_and_printing.py
+++ 00_Fundamentals/02_variables_and_printing.py
@@ -1,11 +1,23 @@
 # Print statements
-print("hello", 5, True)  # hello 5 True
-print("tim")
+# print("hello", 5, True)  # hello 5 True
+# print("tim")
 
-print("hello \n", 5, True)
+# print("hello \n", 5, True)
 
-print("test", 123, True, end="hello")
-print("another line")
+# print("test", 123, True, end="hello")
+# print("another line")
 
 # Variables
-variable = 123
+x = 123
+print(x)
+y = 'hello'
+print(y)
+num = y
+print(num)
+y = x
+print(y)
+print(num)
+x = 4
+print(x)
+
+
