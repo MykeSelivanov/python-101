7d057ece6d17763f6f215b4d0afea1daf75575ac
MykeSelivanov <michael777selivanov@gmail.com>
2024-03-31 22:55:55 -0500

    fundamentals

--- 04_arithmetic_operators.py
+++ 04_arithmetic_operators.py
@@ -1,5 +1,34 @@
-x = 2
+# Addition
+x = 2.0
 y = 3
-
 result = x + y
+print(result) # float + int = float data type
+
+# Subtraction
+x = 10
+y = 3
+result = x - y
+print(result)
+
+# Multiplication
+x = 12
+y = 4
+result = x * y
+print(result)
+
+# Division
+x = 10
+y = 3
+result = x / y
+print(result)
+
+# Exponential
+x = 10
+y = 2
+result = x ** y
+print(result)
+
+a = 2
+b = 0
+result = a**b
 print(result)
