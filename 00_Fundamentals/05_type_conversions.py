0406992502f12beee605a7681c09ca8ec2681ba0
MykeSelivanov <michael777selivanov@gmail.com>
2024-04-03 23:16:55 -0500

    fundamentals

--- 00_Fundamentals/05_type_conversions.py
+++ 00_Fundamentals/05_type_conversions.py
@@ -6,3 +6,15 @@ print(y)
 
 a = float("4.5") + 5
 print(a)
+
+y = bool("4")
+print(y) # true
+y = bool(2)
+print(y) # true
+y = bool(-10)
+print(y) # true
+y = bool("")
+print(y) # false
+y = bool(0)
+print(y) # false
+
