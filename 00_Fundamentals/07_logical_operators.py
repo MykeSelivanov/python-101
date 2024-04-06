88bac3a6a956c24a2f748b057deb87b34f7700a6
MykeSelivanov <michael777selivanov@gmail.com>
2024-04-06 16:39:53 -0500

    fundamentals

--- 00_Fundamentals/07_logical_operators.py
+++ 00_Fundamentals/07_logical_operators.py
@@ -1,5 +1,9 @@
 x = 2
 y = 4
 
-compound = x < y and y + 2 > 3 and y - x >= x
+compound = x < y and y + 2 > 3 and y - x >= x # True True True => True
+print(compound) 
+
+compound = x > y or y + 2 <= 3 or y - x - 1 >= x # False False False => False
 print(compound)
+
