be1ca4b7db01ba4017f3a1357c1f4fa26e9708ce
MykeSelivanov <michael777selivanov@gmail.com>
2024-04-07 21:24:11 -0500

    fundamentals

---
+++ 00_Fundamentals/08_demorgans_law.py
@@ -0,0 +1,12 @@
+# DeMorgan's Law
+# not (x and y) == (not x) or (not y)
+# not (x or y) == (not x) and (not y)
+
+# Example
+# not (w and z or (not w)) => 
+# (not(w and z)) and (not(not w)) =>
+# ((not w) or (not z)) and w =>
+
+'''
+W Z
+'''
