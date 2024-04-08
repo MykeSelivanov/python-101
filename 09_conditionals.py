0b050130985b6e9812fe49a0996108d351018dc7
MykeSelivanov <michael777selivanov@gmail.com>
2024-04-08 15:44:57 -0500

    fundamentals

--- 09_conditionals.py
+++ 09_conditionals.py
@@ -4,4 +4,6 @@ if name == "Tim":
 
 number = float(input("Enter a number below 5: "))
 if number < 5:
-    print("Number ", number, "is less than 5!")
+    print("Number ", number, "is less than 5")
+else:
+    print("Number ", number, "is greater than 5") 
