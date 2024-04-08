ca976dcb4603391bd6052faed30f8d738557c0fe
MykeSelivanov <michael777selivanov@gmail.com>
2024-04-07 21:28:55 -0500

    fundamentals

--- 00_Fundamentals/08_demorgans_law.py
+++ 00_Fundamentals/08_demorgans_law.py
@@ -7,6 +7,12 @@
 # (not(w and z)) and (not(not w)) =>
 # ((not w) or (not z)) and w =>
 
+# Truth table
 '''
 W Z
+---
+F F -> True or True and False -> True or False -> True
+T F -> False or True and True -> False or True -> True
+F T -> True or False and False -> True or False -> True
+T T -> False or False and True -> False or False -> False
 '''
