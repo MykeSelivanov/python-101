2b94768d9c13c01df35818fa29f30cb915e28142
MykeSelivanov <michael777selivanov@gmail.com>
2024-04-11 22:10:07 -0500

    fundamentals

--- 00_Fundamentals/10_lists.py
+++ 00_Fundamentals/10_lists.py
@@ -1,2 +1,13 @@
-x = [1, 2, 3, True, "Hello", 3.14]
+x = [1, 2, 3, True, "World", 3.14]
+print(x[1]) # 2
+print(x) # [1, 2, 3, True, 'World', 3.14]
+
+x[2] = "Hello"
+print(x) # [1, 2, 'Hello', True, 'World', 3.14]
+x.append(False)
+print(x) # [1, 2, 'Hello', True, 'World', 3.14, False]
+
+
+
+
 
