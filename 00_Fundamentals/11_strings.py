5c8f4a880b5a6c56692eb2d4958115a18d03a53d
MykeSelivanov <michael777selivanov@gmail.com>
2024-04-17 22:48:15 -0500

    fundamentals

---
+++ 00_Fundamentals/11_strings.py
@@ -0,0 +1,26 @@
+str = "hello world"
+
+count = str.count("l")
+print(count) # 3
+
+index = str.find("w")
+print(index) # 6
+
+upper = str.upper()
+print(upper) # HELLO WORLD
+
+lower = str.lower()
+print(lower) # hello world
+
+str1 = "test"
+print(str1.capitalize()) # Test
+
+contains = "hel" in str
+print(contains) # True
+
+str2 = "21"
+if str2.isdigit():
+    x = int(str2)
+    print(type(x))
+
+
