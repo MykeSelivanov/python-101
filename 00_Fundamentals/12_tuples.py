8c89affdc749197a16e597af13b942fde97bf500
MykeSelivanov <michael777selivanov@gmail.com>
2024-04-25 23:11:12 -0500

    fundamentals

--- 00_Fundamentals/12_tuples.py
+++ 00_Fundamentals/12_tuples.py
@@ -8,5 +8,14 @@ contains = 2 in x
 print(contains)
 
 # nested tuple
-y = (1, 2, (1, 2, 3), 3, 4, 5)
+y = (1, 2, (1, 2, 'Hello'), 3, 4, True, [])
 print(y)
+print(y[2][2])
+
+a = (1,2)
+b = (10,11)
+combined = a + b
+print(combined) # (1, 2, 10, 11)
+multiplied = a * 2 # (1, 2, 1, 2)
+print(multiplied)
+
