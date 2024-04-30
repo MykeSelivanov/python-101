33acbf021b819f8fcb3e08360258a54f03e1be30
MykeSelivanov <michael777selivanov@gmail.com>
2024-04-29 22:47:00 -0500

    fundamentals

--- 00_Fundamentals/13_for_loops.py
+++ 00_Fundamentals/13_for_loops.py
@@ -6,3 +6,12 @@ for i in range(10, 15):
 
 for i in range(20, 35, 5):
     print(i)
+
+result = 0
+for i in range(1, 11):
+    result += i
+print("Result: ", result)
+
+lst = [1, 2, 3, 4, 5, True, "Hello"]
+for i in range(len(lst)):
+    print(lst[i])
