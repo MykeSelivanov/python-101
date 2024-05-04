1fd113940067ce57227d1552ac709eacb007f85d
MykeSelivanov <michael777selivanov@gmail.com>
2024-05-03 22:49:45 -0500

    fundamentals

---
+++ 00_Fundamentals/14_while_loops.py
@@ -0,0 +1,23 @@
+x = 0
+while x < 3:
+    print(x)
+    x += 1
+
+num = input("Enter an integer: ")
+while not num.isdigit():
+    num = input("Enter an integer: ")
+
+while True:
+    num = input("Enter another integer: ")
+    if num.isdigit():
+        break
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+result = 0
+idx = 0
+while result < 9:
+    num = lst[idx]
+    result += num
+    print(num)
+    idx += 14
+
