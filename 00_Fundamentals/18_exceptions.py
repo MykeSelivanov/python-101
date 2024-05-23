cdb29c9e10b731ea1a8181a0e4afa24af1a87498
MykeSelivanov <michael777selivanov@gmail.com>
2024-05-22 21:54:53 -0500

    fundamentals

--- 00_Fundamentals/18_exceptions.py
+++ 00_Fundamentals/18_exceptions.py
@@ -4,3 +4,19 @@ except ValueError as e:
     print("Exception!", e)
 
 print("Done")
+
+try:
+    2 / 0
+except ValueError as e:
+    print("Value Exception!", e)
+except ZeroDivisionError as e:
+    print("Zero Div Exception!", e)
+
+print("Done 2")
+
+try: 
+    int("test")
+except Exception as e:
+    print("Exception!!!", e)
+finally:
+    print("Finally done!")
