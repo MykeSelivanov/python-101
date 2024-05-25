6e3333e012239b25876aac4d989daa172f719e91
MykeSelivanov <michael777selivanov@gmail.com>
2024-05-25 15:04:45 -0500

    fundamentals

---
+++ 00_Fundamentals/19_functions.py
@@ -0,0 +1,13 @@
+def print_value(value):
+    print(value)
+
+print_value("hello")
+print_value(1)
+print_value(True)
+
+def add_5(x, y):
+    result = x + y + 5
+    return result
+
+result = add_5(10, 20)
+print(result)
