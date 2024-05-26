8c29ee0c5b324c34c8809c13b38dc81621499942
MykeSelivanov <michael777selivanov@gmail.com>
2024-05-26 12:58:39 -0500

    fundamentals

--- 00_Fundamentals/19_functions.py
+++ 00_Fundamentals/19_functions.py
@@ -11,3 +11,21 @@ def add_5(x, y):
 
 result = add_5(10, 20)
 print(result)
+
+x, y, z = 1, 2, -10
+def get_negative_sum(x, y, z):
+    result = x + y + z
+    if result < 0:
+        return result
+    return 1
+
+print(get_negative_sum(x, y, z))
+
+def new_range(start = 0, stop = 5):
+    while start <= stop:
+        print(start)
+        start += 1
+new_range()
+new_range(3)
+new_range(4, 7)
+new_range(stop = 2)
