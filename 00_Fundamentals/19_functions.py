7c91d56f7027785a9ea2a8cb34466fb048cb1dcf
MykeSelivanov <michael777selivanov@gmail.com>
2024-05-27 22:29:49 -0500

    fundamentals

--- 00_Fundamentals/19_functions.py
+++ 00_Fundamentals/19_functions.py
@@ -29,3 +29,30 @@ new_range()
 new_range(3)
 new_range(4, 7)
 new_range(stop = 2)
+
+def return_values(x, y, z):
+    return x + 1, y + 1, z + 1
+print(return_values(2, 3, 4)) # (3, 4, 5)
+
+def remove_chars(base, chars):
+    new_string = base
+    for char in chars:
+        new_string = new_string.replace(char, "")
+    
+    return new_string
+
+result = remove_chars("hello world", "l")
+print(result) # heo word
+
+def sum_lists(list1, list2):
+    def sum_list(lst):
+        total = 0
+        for num in lst:
+            total += num
+        return total
+    list1_sum = sum_list(list1)
+    list2_sum = sum_list(list2)
+    return list1_sum, list2_sum
+
+sum1, sum2 = sum_lists([1,2,3,4,5], [0,10,20,30,40])
+print(sum1, sum2) #15 100
