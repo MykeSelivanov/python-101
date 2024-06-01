ff8ecce9ee52b2bc2907ac84dbaa105c4870cdd1
MykeSelivanov <michael777selivanov@gmail.com>
2024-05-31 22:56:55 -0500

    fundamentals

--- 00_Fundamentals/20_mutability.py
+++ 00_Fundamentals/20_mutability.py
@@ -10,3 +10,16 @@ print(y) # [1, 2, 3]
 x.remove(1)
 print(y) # [2, 3]
 
+a = 1
+b = a
+print(id(a)) # 4397133536
+print(id(b)) # 4397133536 both a and b point to the same object
+
+x = []
+def func(lst, x):
+    lst.append(x)
+    print(lst)
+
+a = []
+func(a, 2) # [2]
+print(a) # [2] - because a was mutated in the function call
