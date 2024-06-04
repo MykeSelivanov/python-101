f871e5fce3175d4035903ba3e2d048d35c309d51
MykeSelivanov <michael777selivanov@gmail.com>
2024-06-03 22:35:26 -0500

    fundamentals

--- 00_Fundamentals/20_mutability.py
+++ 00_Fundamentals/20_mutability.py
@@ -24,3 +24,22 @@ a = []
 func(a, 2) # [2]
 print(a) # [2] - because a was mutated in the function call
 
+d = {}
+d["k"] = "v"
+a = d
+a["a"] = "b"
+print(d, a) # {'k': 'v', 'a': 'b'} {'k': 'v', 'a': 'b'}
+print(d is a) # True
+
+s1 = set()
+s2 = s1
+s1.add(1)
+s2.add(2)
+print(s1 is s2) # True
+
+# Copy a mutable object
+a = [1, 2, 3]
+b = a[:]
+a.append(4)
+print(a, b) # [1, 2, 3, 4] [1, 2, 3]
+print(a is b) # False
