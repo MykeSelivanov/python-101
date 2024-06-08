c92c347920a551c8ebe204c71eb068c8a0ceb008
MykeSelivanov <michael777selivanov@gmail.com>
2024-06-08 13:24:56 -0500

    fundamentals

--- 00_Fundamentals/21_scope.py
+++ 00_Fundamentals/21_scope.py
@@ -2,4 +2,14 @@ def foo():
     x = 0
 
 foo()
-print(x) # NameError: name 'x' is not defined
+# print(x) # NameError: name 'x' is not defined
+
+inp = int(input(("Enter a number: ")))
+if inp > 5: 
+    value  = "Greater than 5!"
+else: 
+    value = "Not greater than 5!"
+
+print(value)
+
+
