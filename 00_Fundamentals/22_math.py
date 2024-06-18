f708515c4a86659d5c9293e722dae2646f4b2958
MykeSelivanov <michael777selivanov@gmail.com>
2024-06-17 22:14:44 -0500

    fundamentals

---
+++ 00_Fundamentals/22_math.py
@@ -0,0 +1,18 @@
+# absolute value
+print(abs(-10)) # 10
+
+lst = [1,2,3,4,5]
+print(max(lst)) # 5
+print(max(1,10, -15)) # 10
+print(min(lst)) # 1
+print(min(-1, -20, 34, 56, -11)) # -20
+
+x = max("a","b")
+print(x) # b
+
+print(sum(lst)) # 15
+print(round(3.14)) # 3
+
+# Math module
+import math
+
