8fd50caa83d18742b11f7852c36c347638e18779
MykeSelivanov <michael777selivanov@gmail.com>
2024-06-20 22:45:12 -0500

    fundamentals

--- 00_Fundamentals/23_sorting.py
+++ 00_Fundamentals/23_sorting.py
@@ -11,3 +11,13 @@ lst.sort(reverse=True)
 print(lst) # [4, 4, 3, 3, 3, 2, 2, 1, 1]
 
 tup = (2, 1, 3, 4, 2, 3, 4, 1, 4, 3, 2)
+
+# Sorting by key
+lst3 = [[1,-2], [3,4], [5,-6], [-1,-2], [0,0], [-1,-2], [4,1], [2,1]]
+lst3.sort() # will sort by the first value [[1,...]]
+print(lst3)
+
+def sort_second_index(item):
+    return item[1]
+lst3.sort(reverse=True, key=sort_second_index) # will sort by the second value [[...,1]]
+print(lst3)
