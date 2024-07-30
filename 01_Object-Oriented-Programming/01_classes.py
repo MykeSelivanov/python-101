c53dcf9b92b1a102e2b8ec180376c6925c585ec6
MykeSelivanov <michael777selivanov@gmail.com>
2024-07-29 22:15:00 -0500

    OOP

--- 01_Object-Oriented-Programming/01_classes.py
+++ 01_Object-Oriented-Programming/01_classes.py
@@ -2,8 +2,18 @@ class Person:
     def __init__(self, x) -> None:
         print(x)
 
-p1 = Person(2)
-p2 = Person(123)
-print(p1)
-print(type(p1))
+p1 = Person(2) # 2
+p2 = Person(123) # 123
+print(p1) # <__main__.Person object at 0x10a65de20>
+print(type(p1)) # <class '__main__.Person'>
+
+
+class ExtraPerson:
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
+
+ep1 = ExtraPerson("Jim", 21)
+print(ep1.name, ep1.age) # Jim 21
+
 
